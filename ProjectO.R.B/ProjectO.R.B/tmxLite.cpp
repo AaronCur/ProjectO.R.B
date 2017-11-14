f2a24a7bfaf85b3af8e47ba16f558a595109571f
JackDalton3110 <c00205943@itcarlow.ie>
2017-11-14 11:55:44 +0000

    tmxLite added

---
+++ ProjectO.R.B/ProjectO.R.B/tmxLite.cpp
@@ -0,0 +1,67 @@
+//#pragma comment(lib,"libtmxlite-s-d.lib") 
+
+#include <tmxlite\Map.hpp>
+#include <iostream>
+
+int main1()
+{
+	
+	tmx::Map map;
+
+	if (map.load("./resources/Map.tmx"))
+	{
+		std::cout << "Loaded Map version: " << map.getVersion().upper << ", " << map.getVersion().lower << std::endl;
+
+		const auto& mapProperties = map.getProperties();
+		std::cout << "Map has " << mapProperties.size() << " properties" << std::endl;
+		for (const auto& prop : mapProperties)
+		{
+			std::cout << "Found property: " << prop.getName() << std::endl;
+			std::cout << "Type: " << int(prop.getType()) << std::endl;
+		}
+
+		std::cout << std::endl;
+
+		const auto& layers = map.getLayers();
+		std::cout << "Map has " << layers.size() << " layers" << std::endl;
+		for (const auto& layer : layers)
+		{
+			std::cout << "Found Layer: " << layer->getName() << std::endl;
+			std::cout << "Layer Type: " << int(layer->getType()) << std::endl;
+
+			if (layer->getType() == tmx::Layer::Type::Object)
+			{
+				const auto& objects = dynamic_cast<tmx::ObjectGroup*>(layer.get())->getObjects();
+				std::cout << "Found " << objects.size() << " objects in layer" << std::endl;
+				for (const auto& object : objects)
+				{
+					std::cout << "Object " << object.getName() << std::endl;
+					const auto& properties = object.getProperties();
+					std::cout << "Object has " << properties.size() << " properties" << std::endl;
+					for (const auto& prop : properties)
+					{
+						std::cout << "Found property: " << prop.getName() << std::endl;
+						std::cout << "Type: " << int(prop.getType()) << std::endl;
+					}
+				}
+			}
+
+			const auto& properties = layer->getProperties();
+			std::cout << properties.size() << " Layer Properties:" << std::endl;
+			for (const auto& prop : properties)
+			{
+				std::cout << "Found property: " << prop.getName() << std::endl;
+				std::cout << "Type: " << int(prop.getType()) << std::endl;
+			}
+		}
+	}
+	else
+	{
+		std::cout << "Failed loading map" << std::endl;
+	}
+
+	std::cout << std::endl << "Press return to quit..." << std::endl;
+	std::cin.get();
+
+	return 0;
+}
