7f461827dc9e971a0d6a10f05da1a579cf86ce62
JackDalton3110 <c00205943@itcarlow.ie>
2017-11-21 14:05:13 +0000

    Map

--- ProjectO.R.B/ProjectO.R.B/resources/tiles.tsx
+++ ProjectO.R.B/ProjectO.R.B/resources/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Tiles" tilewidth="71" tileheight="71" tilecount="156" columns="12">
- <image source="images/tiles_spritesheet.png" width="914" height="936"/>
+<tileset name="tiles" tilewidth="70" tileheight="70" tilecount="144" columns="12">
+ <image source="images/tiles_spritesheet.png" width="888" height="909"/>
 </tileset>
--- ProjectO.R.B/ProjectO.R.B/resources/untitled.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Tiles" tilewidth="70" tileheight="70" tilecount="169" columns="13">
- <image source="images/tiles_spritesheet.png" width="914" height="936"/>
-</tileset>
